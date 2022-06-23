aed82df74d9bf45e5527cea163d7695d45de319d
DoniyorAkramovich <berd.doniyor@gmail.com>
2022-06-23 15:09:32 +0500

    Initial Commit

---
+++ FacebookTimeline/FacebookTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookTimelineApp.swift
+//  FacebookTimeline
+//
+//  Created by Doniyor on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FacebookTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
